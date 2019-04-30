71fa93556b14b8f1f9709160ba7534fa3b922083
Taha Eghtesad <tahaeghtesad@gmail.com>
2019-04-30 17:46:36 -0500

    add mpirun script

---
+++ mpirun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for j in 2 4 8 16 32 64 128 256
+  do
+    mytime="$(TIMEFORMAT='%3R'; time (mpirun --mca btl_vader_backing_directory /tmp -np $j ./a.out) 2>&1 1>/dev/null)"
+#            echo $n,$j,$mytime >> plot.csv
+    printf "$mytime,"
+done
